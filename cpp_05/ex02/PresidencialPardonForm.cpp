fb0fe36602044b42a599c9fb53129badfe285e8f
Franakin <fddp2008@hotmail.com>
2023-05-19 16:21:38 +0200

    finished ex02, main missing

--- cpp_05/ex02/PresidencialPardonForm.cpp
+++ cpp_05/ex02/PresidencialPardonForm.cpp
@@ -6,32 +6,30 @@
 /*   By: fpurdom <fpurdom@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/16 16:21:42 by fpurdom       #+#    #+#                 */
-/*   Updated: 2023/05/18 16:04:45 by fpurdom       ########   odam.nl         */
+/*   Updated: 2023/05/19 16:18:48 by fpurdom       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PresidencialPardonForm.hpp"
 
-//defaults
-PresidencialPardonForm::PresidencialPardonForm(void)
-{
-	std::cout << "Default PresidencialPardonForm constructor" << std::endl;
-}
+//defaults---------------------------------------------------------------------------------------------------------------------------------------------------
+PresidencialPardonForm::PresidencialPardonForm(const std::string newTarget) : AForm("PresidencialPardonForm", 25, 5), target(newTarget){}
 
-PresidencialPardonForm::PresidencialPardonForm(const PresidencialPardonForm &copy)
-{
-	std::cout << "Copy PresidencialPardonForm constructor" << std::endl;
-}
+PresidencialPardonForm::PresidencialPardonForm(void) : AForm("PresidencialPardonForm", 25, 5), target("N/A"){}
 
-PresidencialPardonForm::~PresidencialPardonForm(void)
-{
-	std::cout << "PresidencialPardonForm deconstructor" << std::endl;
-}
+PresidencialPardonForm::PresidencialPardonForm(const PresidencialPardonForm &copy) : AForm("PresidencialPardonForm", 25, 5), target(copy.target){}
 
+PresidencialPardonForm::~PresidencialPardonForm(void){}
+
+//operators---------------------------------------------------------------------------------------------------------------------------------------------------
 PresidencialPardonForm	&PresidencialPardonForm::operator=(const PresidencialPardonForm &copy)
 {
-	//do the copy
+	(void)copy;
 	return (*this);
 }
 
-//member functions
+//member functions---------------------------------------------------------------------------------------------------------------------------------------------------
+void	PresidencialPardonForm::execute(const Bureaucrat &executor) const
+{
+	std::cout << this->target << " has been pardoned by Zaphod Beeblebrox!" << std::endl;
+}
