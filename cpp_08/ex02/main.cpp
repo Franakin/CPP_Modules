4272a76c1a57d273cf25359f41a87280689b1839
Franakin <fddp2008@hotmail.com>
2023-06-04 12:55:18 +0200

    better test

--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fpurdom <fpurdom@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/04 11:25:35 by fpurdom       #+#    #+#                 */
-/*   Updated: 2023/06/04 12:41:58 by fpurdom       ########   odam.nl         */
+/*   Updated: 2023/06/04 12:54:13 by fpurdom       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,40 @@ int main(void)
 
 		it4--;
 		std::cout << "\n//List\nStart: " << *it3 << "\nEnd: " << *it4 << std::endl;
+	}
+	std::cout << "\n\033[1;46m--REVERSE ITERATORS--\033[0m\n" << std::endl;
+	{
+		MutantStack<int> 					mStack;
+		MutantStack<int>::reverse_iterator	it1, it2;
+
+		mStack.push(1);
+		mStack.push(2);
+		mStack.push(3);
+		mStack.push(4);
+		mStack.push(5);
+		mStack.push(6);
+		
+		it1 = mStack.rbegin();
+		it2 = mStack.rend();
+
+		it2--;
+		std::cout << "//MutantStack\nRStart: " << *it1 << "\nREnd: " << *it2 << std::endl;
+
+		std::list<int> 						list;
+		std::list<int>::reverse_iterator	it3, it4;
+
+		list.push_back(1);
+		list.push_back(2);
+		list.push_back(3);
+		list.push_back(4);
+		list.push_back(5);
+		list.push_back(6);
+		
+		it3 = list.rbegin();
+		it4 = list.rend();
+
+		it4--;
+		std::cout << "\n//List\nRStart: " << *it3 << "\nREnd: " << *it4 << std::endl;
 	}
 	std::cout << std::endl;
 	atexit(f);
